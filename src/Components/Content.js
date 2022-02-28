b6957f8b9128e9521b56e89441b4b1ae4d50ffda
vipin <vipinchandra555@gmail.com>
2022-02-28 23:11:35 +0530

    styled-components added

---
+++ src/Components/Content.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 2px solid black;
+`;
+
+function Content() {
+  return <Container>Content</Container>;
+}
+
+export default Content;
