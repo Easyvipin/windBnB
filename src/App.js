ff4bd0f3c66ab9be915bf01a5b02d19fd54ae10a
vipin <vipinchandra555@gmail.com>
2022-02-28 23:30:30 +0530

    added basic layout-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Content from "Components/Content";
+import styled from "styled-components";
+import Header from "Components/Header";
+import SubHeader from "Components/SubHeader";
+
+const LayoutContainer = styled.div`
+  padding: 3rem 6rem;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <LayoutContainer>
+      <Header />
+      <SubHeader />
       <Content />
-    </div>
+    </LayoutContainer>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  margin-bottom: 5rem;
+  border: 2px solid black;
+`;
+
 function Header() {
-  return <div>Header</div>;
+  return <HeaderContainer>Header</HeaderContainer>;
 }
 
 export default Header;
